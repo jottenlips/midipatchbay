5fd2b88a8540590c0c592c35a5ce1add395800f6
John Ottenlips <jottenlips@gmail.com>
2020-07-28 20:51:27 -0500

    Updates

---
+++ precache-manifest.9c785b9cbd26dc2be5173480cdbba79d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30442025e0626454c6889e7cc3f24557",
+    "url": "/index.html"
+  },
+  {
+    "revision": "221fbc9d4cb4b9da6f9a",
+    "url": "/static/css/main.bfa2a90d.chunk.css"
+  },
+  {
+    "revision": "f3b76420ae3a7c03b156",
+    "url": "/static/js/2.8a105a99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8a105a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221fbc9d4cb4b9da6f9a",
+    "url": "/static/js/main.5ae08548.chunk.js"
+  },
+  {
+    "revision": "19eafc09b1d54adcb8f1",
+    "url": "/static/js/runtime-main.2f318f1b.js"
+  }
+]);
